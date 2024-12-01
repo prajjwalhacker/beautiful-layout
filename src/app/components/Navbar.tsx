a0275aff330522baf9d49573bff07029731c6b5d
prajjwalsoni123 <prajjwalsoni123@gmail.com>
2024-12-01 16:43:33 +0530

    svg addition and layout modification

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import donutI from '../../../public/images/donut.svg';
+import Image from 'next/image';
 
 function Navbar() {
   const navbarList = [
@@ -27,6 +29,9 @@ function Navbar() {
   return (
     <div className='navbar'>
         <div className='navbar-left'>
+        <div className='navbar-items'>
+          <Image src={donutI} width={40} height={40} alt='donut'/>
+        </div>
           {navbarList.map((item, index) => {
              return ( 
                 <div className='text-3xl navbar-items' key={index}>
