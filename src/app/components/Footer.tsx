ba1349f11533a87e8c2cc91608031c33aa7e536e
prajjwalsoni123 <prajjwalsoni123@gmail.com>
2024-12-02 02:26:33 +0530

    social icons chages

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import { FaFacebook, FaTwitter, FaInstagram } from 'react-icons/fa';
+
+
 
 function Footer() {
   return (
     <div className='footer'>
         Crafted with love - by Prajjwal Soni 💖
+        <div className='social-icons-container'>
+      <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
+        <FaFacebook style={{ width: '30px', height: '30px' }} className='icon'/>
+      </a>
+      <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
+        <FaTwitter style={{ width: '30px', height: '30px' }} className='icon'/>
+      </a>
+      <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
+        <FaInstagram style={{ width: '30px', height: '30px' }} className='icon'/>
+      </a>
+    </div>
     </div>
   )
 }
