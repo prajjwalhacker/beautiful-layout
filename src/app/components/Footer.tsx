dc68b484c85cbd8ead456a660254fe58fbb48717
prajjwalsoni123 <prajjwalsoni123@gmail.com>
2024-12-02 12:18:00 +0530

    linkedi icon addition

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { FaFacebook, FaTwitter, FaInstagram } from 'react-icons/fa';
+import { FaFacebook, FaInstagram, FaLinkedin } from 'react-icons/fa';
 
 
 
@@ -8,14 +8,14 @@ function Footer() {
     <div className='footer'>
         Crafted with love - by Prajjwal Soni 💖
         <div className='social-icons-container'>
-      <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
+      <a href="https://www.facebook.com/prajjwal.soni/" target="_blank" rel="noopener noreferrer">
         <FaFacebook style={{ width: '30px', height: '30px' }} className='icon'/>
-      </a>
-      <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
-        <FaTwitter style={{ width: '30px', height: '30px' }} className='icon'/>
       </a>
       <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
         <FaInstagram style={{ width: '30px', height: '30px' }} className='icon'/>
+      </a>
+      <a href="https://www.linkedin.com/in/prajjwal-soni-5b0741291/" target="_blank" rel="noopener noreferrer">
+        <FaLinkedin style={{ width: '30px', height: '30px' }} className='icon'/>
       </a>
     </div>
     </div>
