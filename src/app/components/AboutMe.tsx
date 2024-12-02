dbea12e55aecb428c2347342b6de299ab0d5a6cc
prajjwalsoni123 <prajjwalsoni123@gmail.com>
2024-12-02 15:00:23 +0530

    image for open graph

--- src/app/components/AboutMe.tsx
+++ src/app/components/AboutMe.tsx
@@ -16,7 +16,7 @@ function AboutMe() {
         <div className="title">
   <p>👋 Hello, I am <span className="name">Prajjwal</span>, Senior Fullstack Engineer at Gurgaon based startup <span className="company">Fairdeal.market</span>, with a passion for technology, startups, and continuous learning. 🚀</p>
   <p>My career is built on a solid foundation of technical expertise and leadership skills. Proficient in a variety of programming languages like <span className="tech">Javascript</span>, <span className="tech">NodeJS</span>, <span className="tech">NextJS</span>, <span className="tech">Rubyonrails</span>, I have successfully delivered projects that leverage cutting-edge technologies. 🛠️</p>
-  <p>I have mastered <span className="tech">Javascript</span>, <span className="tech">NodeJS</span>, <span className="tech">NextJS</span> technologies. My familiarity with databases like <span className="db">MySQL</span>, <span className="db">PostgreSQL</span>, <span className="db">MongoDB</span> adds depth to my skill set. 📚</p>
+  <p>I have mastered <span className="tech">Javascript</span>, <span className="tech">NodeJS</span>, <span className="tech">Typescript</span>,<span className="tech">NextJS</span> technologies. My familiarity with databases like <span className="db">MySQL</span>, <span className="db">PostgreSQL</span>, <span className="db">MongoDB</span> adds depth to my skill set. 📚</p>
   <p>Beyond the world of code, I love fitness, travelling, and gaming.</p>
 </div>
     </div>
