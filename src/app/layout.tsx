ba9776fe45ba3aa8acbb1112b7ebb227947923cb
prajjwalsoni123 <prajjwalsoni123@gmail.com>
2024-12-02 16:20:51 +0530

    meta tags added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,9 @@ import type { Metadata } from "next";
 export const metadata: Metadata = {
   title: "Prajjwal's Soni Blog App",
   description: "Here i will post everything related to Tech and software engineering and how to reach to the top 1 % of software engineers",
+  openGraph: {
+    images: 'public\images\prajjwal-soni123.jpeg'
+  }
 };
 
 export default function RootLayout({
